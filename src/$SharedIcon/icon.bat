ee573e25fbca0cd28238d7f7dbef4bb1070de70f
o7q <jev.o7q@gmail.com>
2022-10-01 23:54:40 -0500

    Remove $SharedIcon

--- src/$SharedIcon/icon.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-windres "icon.rc" -O coff -o "icon.res"
-echo.
-pause
--- src/transit_operator/compile_shipping.bat
+++ src/transit_operator/compile_shipping.bat
@@ -1,4 +1,4 @@
 @echo off
-g++ "..\$SharedIcon\icon.res" *.cpp -Wall -O2 -o "discordrc_transit_operator.exe"
+g++ *.cpp -Wall -O2 -o "discordrc_transit_operator.exe"
 echo.
 pause
